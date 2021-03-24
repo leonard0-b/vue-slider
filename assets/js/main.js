dcc7a088237543198fff253186e12d3559332707
Leonardo <leonardo.bongianni@gmail.com>
2021-03-24 14:20:18 +0100

    Slider posizionato

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+var app = new Vue (
+  {
+    el: "#root",
+      data: {
+        index: 0,
+        imgs: [
+          "./assets/img/img0.jpg",
+          "./assets/img/img1.jpg",
+          "./assets/img/img2.jpg",
+          "./assets/img/img7.jpg",
+          "./assets/img/img10.jpg",
+        ]
+
+
+      }
+
+
+
+
+
+
+
+
+
+})
