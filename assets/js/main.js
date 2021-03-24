3bd0abd5ba1f0ee379da1ba2d4c680158aba09f0
Leonardo <leonardo.bongianni@gmail.com>
2021-03-24 14:43:19 +0100

    Methods next e prev

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,31 @@
 var app = new Vue (
   {
     el: "#root",
-      data: {
-        index: 0,
-        imgs: [
-          "./assets/img/img0.jpg",
-          "./assets/img/img1.jpg",
-          "./assets/img/img2.jpg",
-          "./assets/img/img7.jpg",
-          "./assets/img/img10.jpg",
-        ]
+    data: {
+      index: 0,
+      imgs: [
+        "./assets/img/img0.jpg",
+        "./assets/img/img1.jpg",
+        "./assets/img/img2.jpg",
+        "./assets/img/img7.jpg",
+        "./assets/img/img10.jpg",
+      ],
+    },
+    methods: {
+      next: function() {
+        if (this.index < this.imgs.length - 1) {
+          this.index++
+        } else {
+          this.index = 0
+        }
+      },
 
-
+      prev: function() {
+        if (this.index > 0) {
+          this.index--
+        } else {
+          this.index = this.imgs.length - 1
+        }
       }
-
-
-
-
-
-
-
-
-
+    }
 })
